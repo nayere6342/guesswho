0bc04a7b422eae2628216f4b7bd17ec0bf78cf75
nayere6342 <146861836+nayere6342@users.noreply.github.com>
2026-07-15 12:46:58 +0000

    Wed, Jul 15, 2026, 8:46 AM -04:00

---
+++ pointlog/score.js
@@ -0,0 +1,8 @@
+import { pscore } from "./tracker.js";
+
+            const add = document.getElementById('add');
+                add.addEventListener('click', () => {
+                document.createElement('callcard');
+                console.log("working!");
+                const player = new PScore();
+        });
