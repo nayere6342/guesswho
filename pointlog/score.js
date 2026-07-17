05bcd66fdf7ad8691ae391f54015c6c7e36e60b4
nayere6342 <146861836+nayere6342@users.noreply.github.com>
2026-07-17 20:26:14 +0000

    Fri, Jul 17, 2026, 4:26 PM -04:00

--- pointlog/score.js
+++ pointlog/score.js
@@ -25,6 +25,31 @@ import { pscore } from "./tracker.js";
                 console.log("working!");
         });
 
+            const remove = document.getElementById('remove');
+                remove.addEventListener('click', () => {
+                const card = document.createElement("p");
+                card.className = "pcard";
+                card.innerHTML = "";
+                document.getElementById("cards").appendChild(card);
+                const po = document.createElement("b");
+                po.className = "sc";
+                po.innerHTML = "0";
+                card.appendChild(po);
+                const tb = document.createElement("input");
+                tb.className = "txt";
+                card.appendChild(tb);
+                const bm = document.createElement("button");
+                bm.className = "cba";
+                bm.innerHTML = "+";
+                card.appendChild(bm);
+                const bm2 = document.createElement("button");
+                bm2.className = "cbs";
+                bm2.innerHTML = "-";
+                card.appendChild(bm2);
+                const player = new pscore();
+                console.log("working!");
+        });
+
             const m = document.getElementById('m');
                 m.addEventListener('click', () => {
                     sc =+ 1;
