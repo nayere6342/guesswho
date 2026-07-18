a8bc0088ea2ab6bec73cb8b0bafa8f0c0920f093
nayere6342 <146861836+nayere6342@users.noreply.github.com>
2026-07-18 01:50:23 +0000

    Fri, Jul 17, 2026, 9:50 PM -04:00

--- pointlog/score.js
+++ pointlog/score.js
@@ -1,50 +1,16 @@
 import { pscore } from "./tracker.js";
 
-            const add = document.getElementById('add');
-                add.addEventListener('click', () => {
-                const card = document.createElement("p");
-                card.className = "pcard";
-                card.innerHTML = "";
-                document.getElementById("cards").appendChild(card);
-                const po = document.createElement("b");
-                po.className = "sc";
-                po.innerHTML = "0";
-                let sc = 0;
-                card.appendChild(po);
-                const tb = document.createElement("input");
-                tb.className = "txt";
-                card.appendChild(tb);
-                const bm = document.createElement("button");
-                bm.className = "cba";
-                bm.innerHTML = "+";
-            });
-
-
-                bm.addEventListener('click', () => {
-                    sc += 1;
-                    po.innerHTML = sc;
-                    console.log(sc);
-                    console.log("point added!");
-                });
-
-                card.appendChild(bm);
-
-                const bm2 = document.createElement("button");
-                bm2.className = "cbs";
-                bm2.innerHTML = "-";
-
-                bm2.addEventListener('click', () => {
-                        sc -= 1;
-                        po.innerHTML = sc;
-                        console.log(sc);
-                        console.log("point loss!");
-                });
-
-                card.appendChild(bm2);
-
-
-            const remove = document.getElementById('remove');
-                remove.addEventListener('click', () => {
-                    document.getElementById("cards").lastElementChild.remove();
-                    console.log("working!");
-        });
+const add = document.getElementById('add');
+    add.addEventListener('click', () => {
+        const card = document.createElement("p");
+        card.className = "pcard";
+        card.innerHTML = "";
+        document.getElementById("cards").appendChild(card);
+        const po = document.createElement("b");
+        po.className = "sc";
+        po.innerHTML = "0";
+        card.appendChild(po);
+        const tb = document.createElement("input");
+        tb.className = "txt";
+        card.appendChild(tb);
+        const bm = document.createElement("button"); bm.className = "cba"; bm.innerHTML = "+"; card.appendChild(bm); const bm2 = document.createElement("button"); bm2.className = "cbs"; bm2.innerHTML = "-"; card.appendChild(bm2); const player = new pscore(); console.log("working!"); }); const remove = document.getElementById('remove'); remove.addEventListener('click', () => { document.querySelector(".pcard").remove(); console.log("working!"); }); const m = document.getElementById('m'); let sc = 0; m.addEventListener('click', () => { sc += 1; document.getElementById("tr").innerHTML = sc; console.log(sc); console.log("point added!"); });
